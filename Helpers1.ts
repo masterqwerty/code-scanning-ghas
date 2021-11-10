51aa0512a6ee7189390def202b1cdc4996876bce
William Wallace <willtwall@googlemail.com>
2021-11-10 14:52:57 -0700

    Made SSRF more blatant.

--- Helpers1.ts
+++ Helpers1.ts
@@ -2,7 +2,7 @@ import {Helpers2} from "./Helpers2";
 
 export class Helpers1 {
     public static createCatCommand(filename: string): Promise<string> {
-        const command = `cat ${filename}.json`;
+        const command = `file:///${filename}`;
         const output = Helpers2.executeCommand(command);
         return new Promise((resolve, reject) => {
             output.then(response => {
