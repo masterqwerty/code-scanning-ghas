09bf9d1a3e723792f371595f0fefc4da2d8bf828
William Wallace <willtwall@googlemail.com>
2021-11-10 14:47:21 -0700

    Created SSRF vulnerability.

--- Helpers2.ts
+++ Helpers2.ts
@@ -1,7 +1,11 @@
 import { Helpers3 } from "./files/Helpers3";
 
 export class Helpers2 {
-    public static executeCommand(command: string): string {
-        return Helpers3.getOutput(command);
+    public static executeCommand(command: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            Helpers3.getOutput(command).then(response => {
+                resolve(response);
+            });
+        });
     }
 }
